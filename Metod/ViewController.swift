90c8e1d9fc8815054c98802692e159b06aa96259
Maksim Tomilov <tomaks88@gmail.com>
2019-10-12 19:20:05 +0500

    Initial Commit

---
+++ Metod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Metod
+//
+//  Created by Максим Томилов on 12.10.2019.
+//  Copyright © 2019 Tomily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
